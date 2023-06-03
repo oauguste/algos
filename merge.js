38a665c2215b02bd9dc920f47f46c7598a984c44
oauguste <augusteosnac@gmail.com>
2023-06-03 03:50:08 -0400

    linkList Completed

--- merge.js
+++ merge.js
@@ -38,7 +38,32 @@ class LinkedList {
   getTail() {
     return this.tail ? this.tail : null;
   }
-  getIndex(value) {
+  contains(value) {
+    let currentNode = this.head;
+    while (
+      currentNode !== null &&
+      currentNode.value !== value
+    ) {
+      currentNode = currentNode.nextNode;
+    }
+    if (currentNode === null) {
+      return false;
+    }
+    return true;
+  }
+  at(index) {
+    if (index < 0 || index >= this.size()) {
+      throw new Error("Index out of bounds");
+    }
+    let currentNode = this.head;
+    let currentIndex = 0;
+    while (currentIndex < index) {
+      currentNode = currentNode.nextNode;
+      currentIndex++;
+    }
+    return currentNode;
+  }
+  find(value) {
     let index = 0;
     let currentNode = this.head;
     while (currentNode !== null) {
@@ -71,6 +96,70 @@ class LinkedList {
     this.tail = prevNode;
     return value;
   }
+
+  toString() {
+    let currentNode = this.head;
+    let result = "";
+    while (currentNode !== null) {
+      result += `(${currentNode.value}) --> `;
+      currentNode = currentNode.nextNode;
+    }
+    return result + "NULL";
+  }
+  insertAt(value, index) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+    if (index === 0) {
+      this.prepend(value);
+      return;
+    }
+    if (index < 0 || index > this.size()) {
+      throw new Error("Index out of bounds");
+    }
+    if (index === this.size()) {
+      this.append(value);
+      return;
+    }
+    let currentNode = this.head;
+    let prevNode = null;
+    let currentIndex = 0;
+    while (currentIndex !== index) {
+      prevNode = currentNode;
+      currentNode = currentNode.nextNode;
+      currentIndex++;
+    }
+    newNode.nextNode = currentNode;
+    prevNode.nextNode = newNode;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.size()) {
+      throw new Error("Index out of bounds");
+    }
+    if (index === 0) {
+      this.head = this.head.nextNode;
+      if (!this.head) {
+        this.tail = null;
+      }
+      return;
+    }
+    let currentNode = this.head;
+    let prevNode = null;
+    let currentIndex = 0;
+    while (currentIndex !== index) {
+      prevNode = currentNode;
+      currentNode = currentNode.nextNode;
+      currentIndex++;
+    }
+    prevNode.nextNode = currentNode.nextNode;
+    if (prevNode.nextNode === null) {
+      this.tail = prevNode;
+    }
+  }
 }
 
 class Node {
@@ -85,13 +174,24 @@ newList.append("First Node");
 
 // console.log(newList);
 newList.append("second node");
-// console.log(newList);
+console.log(newList);
 newList.append("third node");
 // console.log(newList);
 newList.append("fourth node");
 // console.log(newList.head.nextNode);
 newList.prepend("time");
-console.log(newList);
-console.log(newList.size());
-console.log(newList.getHead());
-console.log(newList.getIndex("da node"));
+console.log(newList.toString()); // "time --> First Node --> second node --> third node --> fourth node --> NULL"
+console.log(newList.size()); // 5
+console.log(newList.getHead().value); // "time"
+console.log(newList.getTail().value); // "fourth node"
+console.log(newList.contains("third node")); // true
+console.log(newList.contains("fifth node")); // false
+console.log(newList.at(2).value); // "second node"
+console.log(newList.find("third node")); // 3
+console.log(newList.find("fifth node")); // -1
+newList.remove(2);
+console.log(newList.toString()); // "time --> First Node --> third node --> fourth node --> NULL"
+console.log(newList.size()); // 4
+newList.insertAt("inserted node", 2);
+console.log(newList.toString()); // "time --> First Node --> inserted node --> third node --> fourth node --> NULL"
+console.log(newList.size()); // 5
